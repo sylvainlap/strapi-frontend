af782e2272d6f94c669f32ab176dbb840468bd34
SylvainLap <sylvain.lapendry@gmail.com>
2016-03-02 09:28:02 +0100

    style Header

--- client/components/Header/index.jsx
+++ client/components/Header/index.jsx
@@ -1,60 +1,39 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
-import { actions as authActions } from '../../redux/modules/auth';
-
 class Header extends Component {
-  constructor() {
-    super();
-    this._handleClick = this._handleClick.bind(this);
-  }
-
-  _handleClick() {
-    this.props.dispatch(authActions.signoutUser());
-  }
-
   render() {
     const { isAuthenticated } = this.props;
 
     return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="/">strapi frontend</a>
-          </div>
+      <header className="row col-12">
+        <h1><i className="fa fa-comments-o logo"></i>&nbsp;Référents Antibio</h1>
+        {!isAuthenticated && <div>
+          <Link className="user-menu lnk-clr" to="/signin">
+            <i className="fa fa-sign-in"></i>&nbsp;S'authentifier
+          </Link>
+          <Link className="user-menu lnk-clr" to="/signup">
+            <i className="fa fa-user-plus"></i>&nbsp;Créer un compte
+          </Link>
+        </div>}
 
-            {!isAuthenticated && <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/signin">
-                  <button type="button" className="btn btn-primary navbar-btn">Sign in</button>
-                </Link>
-              </li>
-              <li>
-                <Link to="/signup">
-                  <button type="button" className="btn btn-primary navbar-btn">Sign up</button>
-                </Link>
-              </li>
-            </ul>}
-
-            {isAuthenticated && <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/createMessage">
-                  <button type="button" className="btn btn-warning navbar-btn">Create message</button>
-                </Link>
-              </li>
-              <li>
-                <button type="button" className="btn btn-danger navbar-btn" onClick={this._handleClick}>Sign out</button>
-              </li>
-            </ul>}
-
-        </div>
-      </nav>
+        {isAuthenticated && <div>
+          <Link className="user-menu lnk-clr" to="/profile">
+            <i className="fa fa-user-md"></i>&nbsp;Dr Perrin
+          </Link>
+          <Link className="user-menu lnk-clr" to="/createMessage">
+            <i className="fa fa-plus"></i>&nbsp;Créer un nouveau message
+          </Link>
+          <Link className="user-menu lnk-clr" to="/signout">
+            <i className="fa fa-sign-out"></i>&nbsp;Se déconnecter
+          </Link>
+        </div>}
+      </header>
     );
   }
 }
 
 Header.propTypes = {
-  dispatch: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
 };
 

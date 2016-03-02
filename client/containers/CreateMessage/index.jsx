8352d0802a4c4d69c0c0463f1cb8145cb19f85aa
SylvainLap <sylvain.lapendry@gmail.com>
2016-03-02 13:36:28 +0100

    style createMessage

--- client/containers/CreateMessage/index.jsx
+++ client/containers/CreateMessage/index.jsx
@@ -13,54 +13,116 @@ class CreateMessage extends Component {
     evt.preventDefault();
 
     const newMessage = {
+      title: this._title.value,
       content: this._content.value,
+      tags: this._tags.value,
     };
     this.props.dispatch(messagesActions.createMessage(newMessage));
   }
 
   render() {
-    const { errorMessage } = this.props;
-
     return (
-      <div>
-        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-        <form className="form-horizontal" onSubmit={this._handleSubmit}>
-          <h2>Create a new message</h2>
-          <div className="form-group">
-            <label htmlFor="textareaContent" className="col-sm-2 control-label">Content</label>
-            <div className="col-sm-10">
-              <textarea
-                className="form-control"
-                rows="3"
-                id="textareaContent"
-                placeholder="Content of your message"
-                ref={(node) => { this._content = node; }} // eslint-disable-line react/jsx-no-bind
-              />
-            </div>
+      <main>
+        <div className="page-logo col-12">
+          <span>
+            <i className="fa fa-pencil"></i>
+          </span>
+        </div>
+        <h1 className="page-title">Nouvelle question</h1>
+        <form className="col-6 col-ol-3" onSubmit={this._handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="title">Titre</label>
+            <input
+              type="text"
+              id="title"
+              ref={(node) => { this._title = node; }} // eslint-disable-line react/jsx-no-bind
+            />
           </div>
-          <div className="form-group">
-            <div className="col-sm-offset-2 col-sm-10">
-              <button type="submit" className="btn btn-success">Create</button>
-            </div>
+          <div className="input-group">
+            <label htmlFor="content">Question</label>
+             <textarea
+               className="col-8"
+               rows="5"
+               id="content"
+               ref={(node) => { this._content = node; }}  // eslint-disable-line react/jsx-no-bind
+             >
+             </textarea>
+          </div>
+          <div className="input-group">
+            <label htmlFor="tags">Tags</label>
+            <input
+              type="text"
+              id="tags"
+              ref={(node) => { this._tags = node; }} // eslint-disable-line react/jsx-no-bind
+            />
+          </div>
+          <div className="input-group">
+            <button className="col-4 col-ol-4">Envoyer</button>
           </div>
         </form>
-      </div>
+
+        <h1 className="page-title">Suggestions</h1>
+        <div className="threads col-12">
+          <table className="col-12">
+            <tr onClick="location.pathname = './thread-open.html'">
+              <td className="user">
+                <span><i>Dr. Combes</i></span>
+              </td>
+              <td className="question">
+                <div className="msg user">
+                  <p>Qu’est-ce qu’un antibiotique ?</p>
+                </div>
+                <div className="msg ref">
+                  <p>
+                    Les antibiotiques sont des médicaments capables d’entraîner
+                    la destruction (effet bactéricide) ou l’arrêt ...
+                  </p>
+                </div>
+                <div className="tags"></div>
+              </td>
+              <td className="info">
+                ouverte : 12/01/16<br /><br />
+                fermée : 01/02/16
+              </td>
+              <td className="status closed">
+                <span><i className="fa fa-check"></i></span>
+              </td>
+            </tr>
+            <tr onClick="location.pathname = './thread-open.html'">
+              <td className="user">
+                <span><i>Dr. Lucas</i></span>
+              </td>
+              <td className="question">
+                <div className="msg user">
+                  <p>Pourquoi utilise-t-on des antibiotiques en élevage sur des animaux qui ne sont pas malades?</p>
+                </div>
+                <div className="msg ref">
+                  <p>
+                    La plupart des animaux de production ou de rente sont
+                    élevés en groupe (volailles, porcs, veaux, bovins)...
+                  </p>
+                </div>
+                <div className="tags">
+                  <a href="#">élevage</a><a href="#">pratique</a>
+                </div>
+              </td>
+              <td className="info">
+                ouverte : 11/01/16<br /><br />
+                mise à jour : 14/01/16
+              </td>
+              <td className="status in-progress">
+                <span><i className="fa fa-ellipsis-h"></i></span>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </main>
     );
   }
 }
 
 CreateMessage.propTypes = {
   dispatch: PropTypes.func.isRequired,
-  errorMessage: PropTypes.string,
 };
 
-function mapStateToProps(state) {
-  const { messages } = state;
-  const { errorMessage } = messages;
-
-  return {
-    errorMessage,
-  };
-}
-
-export default connect(mapStateToProps)(CreateMessage);
+export default connect()(CreateMessage);

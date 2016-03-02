68bc8d721c937269fdcdf622e4b111f6705ca98b
SylvainLap <sylvain.lapendry@gmail.com>
2016-03-02 12:50:39 +0100

    reset error message

--- client/containers/App/index.jsx
+++ client/containers/App/index.jsx
@@ -2,17 +2,38 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import Header from '../../components/Header';
-import { actions as authActions } from '../../redux/modules/auth';
-import { actions as messagesActions } from '../../redux/modules/messages';
+import { actions as errorActions } from '../../redux/modules/errorMessage';
 
 class App extends Component {
+  constructor() {
+    super();
+    this._handleDismissClick = this._handleDismissClick.bind(this);
+  }
+
+  _handleDismissClick(evt) {
+    evt.preventDefault();
+    this.props.dispatch(errorActions.resetErrorMessage());
+  }
+
   render() {
     const { dispatch, children, isAuthenticated, errorMessage } = this.props;
 
     return (
       <div>
         <Header dispatch={dispatch} isAuthenticated={isAuthenticated} />
-        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+        {errorMessage && <div className="logger">
+          <div className="log error col-12">
+            <div className="log-icon">
+              <i className="fa"></i>
+            </div>
+            <div className="log-msg col-10 col-ol-2">
+              <p>
+                {errorMessage}
+                {' '}(<a href="#" onClick={this._handleDismissClick}>X</a>)
+              </p>
+            </div>
+          </div>
+        </div>}
         {children}
       </div>
     );
@@ -27,8 +48,8 @@ App.propTypes = {
 };
 
 function mapStateToProps(state) {
-  const { auth } = state;
-  const { isAuthenticated, errorMessage } = auth;
+  const { auth, errorMessage } = state;
+  const { isAuthenticated } = auth;
 
   return {
     isAuthenticated,
--- client/redux/modules/errorMessage.js
+++ client/redux/modules/errorMessage.js
@@ -2,6 +2,20 @@
 
 const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE';
 
+// Actions creators
+
+function resetErrorMessage() {
+  return {
+    type: RESET_ERROR_MESSAGE,
+  };
+}
+
+// Exposed Actions
+
+export const actions = {
+  resetErrorMessage,
+};
+
 // Reducers (must be pure !)
 
 export default function errorMessage(state = null, action) {
@@ -10,7 +24,7 @@ export default function errorMessage(state = null, action) {
   if (type === RESET_ERROR_MESSAGE) {
     return null;
   } else if (error) {
-    return payload;
+    return payload.message;
   }
 
   return state;

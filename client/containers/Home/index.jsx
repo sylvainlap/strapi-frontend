7bba0b42c1bba02aca767209cdba68c89fcf376c
SylvainLap <sylvain.lapendry@gmail.com>
2016-03-04 13:21:49 +0100

    add search

--- client/containers/Home/index.jsx
+++ client/containers/Home/index.jsx
@@ -1,12 +1,33 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import MessageList from '../../components/MessageList';
 import { actions as messagesActions } from '../../redux/modules/messages';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredMessage: null,
+    };
+    this._handleSearchChange = this._handleSearchChange.bind(this);
+  }
+
   componentWillMount() {
     this.props.dispatch(messagesActions.getMessages());
+  }
+
+  _handleSearchChange(evt) {
+    const filteredMessage = [];
+    _.forEach(this.props.items, (item) => {
+      if (_.includes(item.title, evt.target.value)) {
+        filteredMessage.push(item);
+      }
+    });
+    this.setState({
+      filteredMessage,
+    });
   }
 
   render() {
@@ -24,12 +45,16 @@ class Home extends Component {
         </div>
         <h1 className="page-title">Questions / Réponses</h1>
         <div className="search-input col-12">
-          <input type="text" placeholder="Rechercher ..." />
+          <input
+            type="text"
+            placeholder="Rechercher ..."
+            onChange={this._handleSearchChange}
+          />
           <button>
             <span><i className="fa fa-search"></i></span>
           </button>
         </div>
-        <MessageList messages={items} />
+        <MessageList messages={this.state.filteredMessage || items} />
       </main>
     );
   }
